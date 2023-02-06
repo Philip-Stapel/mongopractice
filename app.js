0d2a1cd6a3439af9be09f06251bfedb4f72582a5
Philip-Stapel <121026486+Philip-Stapel@users.noreply.github.com>
2023-02-06 13:08:18 +0000

    added docker to app

---
+++ app.js
@@ -0,0 +1,78 @@
+const express = require("express");
+const app = express();
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+const port = 3000;
+
+const { MongoClient } = require("mongodb");
+
+const url = "mongodb://mongo:27017";
+const client = new MongoClient(url, { useUnifiedTopology: true });
+const dbName = "addressbook";
+
+async function main() {
+  // Use connect method to connect to the server
+  await client.connect();
+  console.log("Connected successfully to server");
+  const db = client.db(dbName);
+  const collection = db.collection("documents");
+
+  const entriesCollection = db.collection("entries");
+
+  app.post("/entries", async (req, res) => {
+    const entry = {
+      id: req.body.id,
+      name: req.body.name,
+      address: req.body.address,
+      email: req.body.email,
+      age: req.body.age,
+    };
+
+    await entriesCollection.insertOne(entry);
+    if (entry.name.length > 1) {
+      res
+        .status(201)
+        .send(
+          "New entry for " + req.body.name + " has been added! Stay in touch!"
+        );
+    } else {
+      res.status(400).send("Please enter valid data");
+    }
+  });
+
+  app.get("/entries", async (req, res) => {
+    const entries = await entriesCollection.find({}).toArray();
+    if (entries.length === 0) {
+      res.status(404).send("No entries found");
+    } else {
+      res.status(200).send(entries);
+    }
+  });
+
+  app.delete("/entries/:id", async (req, res) => {
+    await entriesCollection.deleteOne({ id: req.params.id });
+    res.status(200).send({ message: "Entry deleted successfully" });
+  });
+
+  app.patch("/entries/:id", async (req, res) => {
+    const result = await entriesCollection.updateOne(
+      { id: req.params.id },
+      { $set: req.body }
+    );
+    if (result.modifiedCount === 0) {
+      res.status(404).send( "no matching records");
+    } else {
+      res.status(200).send("Entry updated successfully");
+    }
+  });
+}
+
+main()
+  .then(() => {
+    app.listen(port, () => {
+      console.log("server is up on port " + port);
+    });
+  })
+  .catch(console.error);
+
+module.exports = app;
---
+++ src/__test__/intergration.test.js
@@ -0,0 +1,166 @@
+const app = require("../../app.js");
+const { MongoClient } = require("mongodb");
+const request = require("supertest");
+
+describe("API Endpoints", () => {
+  let connection;
+  let db;
+
+  beforeAll(async () => {
+    connection = await MongoClient.connect("mongodb://localhost:27017", {
+      useUnifiedTopology: true,
+    });
+    db = connection.db("addressbook");
+    const entriesCollection = db.collection("entries");
+  });
+
+  afterAll(async () => {
+    await connection.close();
+  });
+
+  describe("POST /entries", () => {
+    it("creates a new entry", async () => {
+      const response = await request(app).post("/entries").send({
+        id: "1",
+        name: "Phil",
+        address: "london",
+        email: "phil@example.com",
+        age: 30,
+      });
+      expect(response.statusCode).toBe(201);
+      expect(response.text).toBe(
+        "New entry for Phil has been added! Stay in touch!"
+      );
+    });
+    it("gives a 400 when invalid data is added", async () => {
+      const response = await request(app).post("/entries").send({
+        name: "",
+        address: "orchard street",
+        email: "darry@email.com",
+        age: 30,
+      });
+      expect(response.statusCode).toBe(400);
+      expect(response.text).toBe("Please enter valid data");
+    });
+  });
+  describe("GET /entries", () => {
+    it("gets all the entries", async () => {
+      await db.collection("entries").insertMany([
+        {
+          id: "1",
+          name: "Phil",
+          address: "london",
+          email: "phil@example.com",
+          age: 30,
+        },
+        {
+          id: "2",
+          name: "gavin",
+          address: "canterbury",
+          email: "gavin@example.com",
+          age: 30,
+        },
+      ]);
+      const response = await request(app).get("/entries");
+      expect(response.statusCode).toBe(200);
+      expect(response.body).toHaveLength(2);
+    });
+    it("returns 404 when no entries found", async () => {
+      const response = await request(app).get("/entries");
+      expect(response.statusCode).toBe(404);
+      expect(response.text).toBe("No entries found");
+    });
+  });
+  describe("DELETE /entries", () => {
+    it("deletes specified entry from database and returns 200", async () => {
+      await db.collection("entries").insertMany([
+        {
+          id: "1",
+          name: "Phil",
+          address: "london",
+          email: "phil@example.com",
+          age: 30,
+        },
+        {
+          id: "2",
+          name: "gavin",
+          address: "canterbury",
+          email: "gavin@example.com",
+          age: 30,
+        },
+      ]);
+      const response = await request(app).delete("/entries/2").send();
+      expect(response.statusCode).toBe(200);
+      const entriesCollection = db.collection("entries");
+      const entries = await entriesCollection.find({}).toArray();
+      console.log(entries);
+      expect(entries.length).toEqual(1);
+    });
+  });
+  describe("PATCH /entries", () => {
+    it("edits the correct post", async () => {
+      await db.collection("entries").insertMany([
+        {
+          id: "1",
+          name: "Phil",
+          address: "london",
+          email: "phil@example.com",
+          age: 30,
+        },
+        {
+          id: "2",
+          name: "gavin",
+          address: "canterbury",
+          email: "gavin@example.com",
+          age: 30,
+        },
+      ]);
+      const response = await request(app).patch("/entries/2").send({
+        id: "2",
+        name: "steven",
+        address: "canterbury",
+        email: "gavin@example.com",
+        age: 30,
+      });
+      const entriesCollection = db.collection("entries");
+      const entries = await entriesCollection.find({ id: "2" }).toArray();
+      console.log(entries)
+      expect(entries.length).toBe(1);
+      expect(entries[0].name).toBe("steven")
+    });
+  });
+  it('gives 404 if not records were changed', async () => {
+    await db.collection("entries").insertOne(
+      {
+        id: "1",
+        name: "Phil",
+        address: "london",
+        email: "phil@example.com",
+        age: 30
+      })
+    const response = await request(app).patch("/entries/2").send({
+          id: "2",
+          name: "steven",
+          address: "canterbury",
+          email: "gavin@example.com",
+          age: 30,
+        }
+    )
+    expect(response.status).toBe(404)
+    console.log(response.text)
+    expect(response.text).toEqual("no matching records")
+
+
+
+
+  })
+});
+
+afterEach(async () => {
+  connection = await MongoClient.connect("mongodb://localhost:27017", {
+    useUnifiedTopology: true,
+  });
+  db = connection.db("addressbook");
+  const entriesCollection = db.collection("entries");
+  await entriesCollection.deleteMany({});
+});
